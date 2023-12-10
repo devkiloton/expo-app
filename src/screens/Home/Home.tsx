1d3b172d05c248ae27f9f06633c4c434593d5ef8
devkiloton <devkiloton@gmail.com>
2023-12-10 14:50:52 -0300

    Making scrollable on browser

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { Text } from "react-native-paper";
 import { ScreenBaseContainer } from "../../components/shared";
 import { EventList } from "./components/EventList";
 
 export const Home = React.memo(() => {
-  return (
-    <ScreenBaseContainer>
-      <Text>Home</Text>
-      <EventList />
-    </ScreenBaseContainer>
-  );
+  return <EventList />;
 });
