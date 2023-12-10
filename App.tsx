440b6e7c2e26fdba407c4081698697378c43ba38
devkiloton <devkiloton@gmail.com>
2023-12-10 14:35:44 -0300

    Removing polyfills

--- App.tsx
+++ App.tsx
@@ -2,10 +2,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { PaperProvider } from "react-native-paper";
 import { MainBottomTab } from "./src/routes/BottomTab";
-import { applyGlobalPolyfills } from "./src/polyfills/applyGlobalPolyfills";
 
 export default function App() {
-  applyGlobalPolyfills();
   return (
     <NavigationContainer>
       <PaperProvider>
--- src/polyfills/applyGlobalPolyfills.ts
+++
@@ -1,3 +0,0 @@
-export const applyGlobalPolyfills = () => {
-  global.TextEncoder = require("text-encoding").TextEncoder;
-};
